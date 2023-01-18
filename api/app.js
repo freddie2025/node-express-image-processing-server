8d6bb6b60f1703ca6d58a9307164194159f0629a
Fred James <fredjsam@live.co.uk>
2023-01-18 12:13:55 +0000

    Uppercase Submit button value tag

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const app = express.express();
+const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
 
